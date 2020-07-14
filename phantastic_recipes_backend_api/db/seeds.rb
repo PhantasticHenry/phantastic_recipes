05e2db3db99325804bebf21827f0bf059c49c2db
github username <github email address>
2020-07-13 23:42:17 -0700

    added recipes and ingredients

--- phantastic_recipes_backend_api/db/seeds.rb
+++ phantastic_recipes_backend_api/db/seeds.rb
@@ -9,6 +9,7 @@
 mini_cheesecakes = Recipe.create( title: "Mini Cheesecakes", recipe_link: "https://www.cookingclassy.com/cheesecake-cupcakes/", image_link: "https://www.cookingclassy.com/wp-content/uploads/2013/04/mini-cheesecakes-01.jpg", recipe_origin: "greece", recipe_type: "dessert" )
 mini_cheesecakes.ingredients << [ Ingredient.find_or_create_by(name: "graham crackers"), Ingredient.find_or_create_by(name: "granulated sugar"), Ingredient.find_or_create_by(name: "butter"), Ingredient.find_or_create_by(name: "cream cheese"), Ingredient.find_or_create_by(name: "all purpose flour"), Ingredient.find_or_create_by(name: "eggs"), Ingredient.find_or_create_by(name: "vanilla extract"), Ingredient.find_or_create_by(name: "sour cream"), Ingredient.find_or_create_by(name: "heavy cream") ]
 
+
 chocolate_covered_strawberries = Recipe.create( title: "Chocolate Covered Strawberries", recipe_link: "https://www.cookingclassy.com/chocolate-covered-strawberries/", image_link: "https://www.cookingclassy.com/wp-content/uploads/2020/01/chocolate-covered-strawberries-15-768x1152.jpg", recipe_origin: "america", recipe_type: "dessert" )
 chocolate_covered_strawberries.ingredients << [ Ingredient.find_or_create_by(name: "chocolate chips"), Ingredient.find_or_create_by(name: "fresh strawberries"), Ingredient.find_or_create_by(name: "coconut oil") ]
 
