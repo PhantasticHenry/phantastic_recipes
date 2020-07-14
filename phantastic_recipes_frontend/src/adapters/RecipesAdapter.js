a2b12d71984cf33d5f3b82a88b74a12be1f19a99
github username <github email address>
2020-07-14 14:56:39 -0700

    added get request to fetch recipes

---
+++ phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
@@ -0,0 +1,10 @@
+class RecipesAdapter {
+    constructor() {
+        this.baseUrl = 'http://localhost:3000/recipes'
+    }
+
+    getRecipes() {
+        return fetch(this.baseUrl).then(res => res.json()
+        )
+    }
+}
