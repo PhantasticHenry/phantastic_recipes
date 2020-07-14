264b698c1bf84af1e1d1495e935866c8fb42b75e
github username <github email address>
2020-07-13 19:22:54 -0700

    recipe and ingredients added

--- phantastic_recipes_backend_api/db/seeds.rb
+++ phantastic_recipes_backend_api/db/seeds.rb
@@ -6,4 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-mini_cheesecakes = Recipe.create(title: "Mini Cheesecakes", recipe_link: "https://www.cookingclassy.com/cheesecake-cupcakes/", image_link: "https://www.cookingclassy.com/wp-content/uploads/2013/04/mini-cheesecakes-01.jpg", recipe_origin: "greece", recipe_type: "dessert" )
+mini_cheesecakes = Recipe.create( title: "Mini Cheesecakes", recipe_link: "https://www.cookingclassy.com/cheesecake-cupcakes/", image_link: "https://www.cookingclassy.com/wp-content/uploads/2013/04/mini-cheesecakes-01.jpg", recipe_origin: "greece", recipe_type: "dessert" )
+mini_cheesecakes.ingredients << [ Ingredient.find_or_create_by(name: "graham crackers"), Ingredient.find_or_create_by(name: "granulated sugar"), Ingredient.find_or_create_by(name: "butter"), Ingredient.find_or_create_by(name: "cream cheese"), Ingredient.find_or_create_by(name: "all purpose flour"), Ingredient.find_or_create_by(name: "eggs"), Ingredient.find_or_create_by(name: "vanilla extract"), Ingredient.find_or_create_by(name: "sour cream"), Ingredient.find_or_create_by(name: "heavy cream") ]
+
+chocolate_covered_strawberries = Recipe.create( title: "Chocolate Covered Strawberries", recipe_link: "https://www.cookingclassy.com/chocolate-covered-strawberries/", image_link: "https://www.cookingclassy.com/wp-content/uploads/2020/01/chocolate-covered-strawberries-15-768x1152.jpg", recipe_origin: "america", recipe_type: "dessert" )
+chocolate_covered_strawberries.ingredients << [ Ingredient.find_or_create_by(name: "chocolate chips"), Ingredient.find_or_create_by(name: "fresh strawberries"), Ingredient.find_or_create_by(name: "coconut oil") ]
+
+banana_bread_muffins = Recipe.create( title: "Banana Bread Muffins", recipe_link: "https://www.lovefromtheoven.com/banana-bread-muffins/", image_link: "https://www.lovefromtheoven.com/wp-content/uploads/2018/01/muffin-recipes-9845.webp", recipe_origin: "america", recipe_type: "dessert")
+banana_bread_muffins.ingredients << [ Ingredient.find_or_create_by(name: "all purpose flour"), Ingredient.find_or_create_by(name: "baking powder"), Ingredient.find_or_create_by(name: "salt"), Ingredient.find_or_create_by(name: "ripe bananas"), Ingredient.find_or_create_by(name: "butter"), Ingredient.find_or_create_by(name: "brown sugar"), Ingredient.find_or_create_by(name: "eggs"), Ingredient.find_or_create_by(name: "vanilla"), Ingredient.find_or_create_by(name: "chocolate chips") ]
