ba3443d7742c0482ae8adeec9697d6f1405bc251
github username <github email address>
2020-07-13 21:14:24 -0700

    added show, create, & strong params

--- phantastic_recipes_backend_api/app/controllers/recipes_controller.rb
+++ phantastic_recipes_backend_api/app/controllers/recipes_controller.rb
@@ -5,4 +5,20 @@ class RecipesController < ApplicationController
 
         render json: RecipeSerializer.new(recipes)
     end
+
+    def show 
+        
+    end
+
+    def create
+        recipe = Recipe.new(recipe_params)
+        byebug
+    end
+
+    private
+    
+    def recipe_params 
+        params.require(:recipe).permit(:title, :recipe_link, :image_link, :recipe_origin, :recipe_type)
+    end
+
 end
