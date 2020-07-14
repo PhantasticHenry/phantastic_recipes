55896e087ff69689ac3c4d8860a7a3e1d47d9e77
github username <github email address>
2020-07-13 23:42:41 -0700

    added post route for create action

--- phantastic_recipes_backend_api/config/routes.rb
+++ phantastic_recipes_backend_api/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/ingredients' => 'ingredients#index'
 
   get '/recipes' => 'recipes#index'
+  get '/recipes:ingredient_name' => 'recipes#show'
   post '/recipes' => 'recipes#create'
+
 end
