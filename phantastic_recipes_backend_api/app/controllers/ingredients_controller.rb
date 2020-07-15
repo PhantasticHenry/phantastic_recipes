76f5b62e96f406b4a784dcc2c7618e8e0bae6cf7
github username <github email address>
2020-07-15 14:17:41 -0700

    render ingredients in json format

--- phantastic_recipes_backend_api/app/controllers/ingredients_controller.rb
+++ phantastic_recipes_backend_api/app/controllers/ingredients_controller.rb
@@ -3,6 +3,6 @@ class IngredientsController < ApplicationController
     def index 
         ingredients = Ingredient.all 
 
-        render json: IngredientSerializer.new(ingredients)
+        render json: ingredients
     end
 end
