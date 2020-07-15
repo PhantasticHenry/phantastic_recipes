135c1ca69297b373f83183c12f2dd3c121534d9f
github username <github email address>
2020-07-15 14:14:58 -0700

    inherit ActiveModel::Serializer

--- phantastic_recipes_backend_api/app/serializers/recipe_serializer.rb
+++ phantastic_recipes_backend_api/app/serializers/recipe_serializer.rb
@@ -1,5 +1,4 @@
-class RecipeSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :title, :recipe_link, :image_link, :recipe_origin, :recipe_type
+class RecipeSerializer < ActiveModel::Serializer
+  attributes :id, :title, :recipe_link, :image_link, :recipe_origin, :recipe_type
   has_many :ingredients
 end
