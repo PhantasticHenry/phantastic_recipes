ecce48a5888ed5bec74b8ad3a5a62238ed43c211
github username <github email address>
2020-07-17 12:12:46 -0700

    class property declared

---
+++ phantastic_recipes_frontend/src/components/recipe.js
@@ -0,0 +1,5 @@
+class Recipe {
+    constructor(title, recipe_link, image_link, recipe_origin, recipe_type) {
+        Object.assign(this, { title, recipe_link, image_link, recipe_origin, recipe_type })
+    }
+}
