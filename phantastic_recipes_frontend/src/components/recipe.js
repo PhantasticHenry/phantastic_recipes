9ac09607c3b3d6cb488d02e88c4e38cc21c80828
github username <github email address>
2020-07-17 15:18:18 -0700

    added createRecipeCard function, div & h3 element

--- phantastic_recipes_frontend/src/components/recipe.js
+++ phantastic_recipes_frontend/src/components/recipe.js
@@ -2,4 +2,12 @@ class Recipe {
     constructor(title, recipe_link, image_link, recipe_origin, recipe_type) {
         Object.assign(this, { title, recipe_link, image_link, recipe_origin, recipe_type })
     }
+
+    createRecipeCard() {
+        const recipeCard = document.createElement('div')
+        recipeCard.className = "recipe-card"
+        const recipeTitle = document.createElement('h3')
+        recipeTitle.innerHTML = this.title
+
+    }
 }
