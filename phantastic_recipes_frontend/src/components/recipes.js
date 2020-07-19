0a765c11026a25a446d7f1b58d2d85ec8c1eee23
github username <github email address>
2020-07-19 12:35:13 -0700

    added initBindingAndEvenListers to store all bind and event listeners

--- phantastic_recipes_frontend/src/components/recipes.js
+++ phantastic_recipes_frontend/src/components/recipes.js
@@ -1,15 +1,42 @@
 class Recipes {
     constructor() {
-        this.recipes = []
-        this.adapter = new RecipesAdapter()
-        // this.bindEventListers()
-        this.fetchAndLoadRecipes()
+        this.recipes = [];
+        this.adapter = new RecipesAdapter();
+        this.initBindingsAndEventListeners();
+        this.fetchAndLoadRecipes();
     }
 
     fetchAndLoadRecipes() {
         this.adapter.getRecipes().then(recipes => {
-            console.log(recipes);
-            
+            recipes.forEach(recipe => this.recipes.push(new Recipe(recipe)))                        
+        })
+        .then(() => {
+            this.renderRecipes()
         })
     }
+    
+    initBindingsAndEventListeners() {
+        this.addRecipForm = document.querySelector('#addRecipeForm')
+        this.addRecipForm.addEventListener('submit', this.addRecipe)
+    }
+
+
+    createRecipeIngredientsArray(ingredients) {
+        let ingredientsArray = [];
+        for (const ingredient of ingredients) {
+            ingredientsArray.push(ingredient.name);
+        }
+        return ingredientsArray;        
+    }
+
+    addRecipe(recipe) {
+        recipe.preventDefault()  
+    }
+
+    renderRecipes() {        
+        for (let recipe of this.recipes) {
+            recipe.createRecipeCard()            
+        }
+    }
+
 }
