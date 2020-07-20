b106a8483a91e6c669f82f558517a5a5088d8e9e
github username <github email address>
2020-07-19 18:22:09 -0700

    updated create action to interate through ingredients params, create ingredient if not found and set to ingredeints

--- phantastic_recipes_backend_api/app/controllers/recipes_controller.rb
+++ phantastic_recipes_backend_api/app/controllers/recipes_controller.rb
@@ -2,17 +2,23 @@ class RecipesController < ApplicationController
 
     def index 
         recipes = Recipe.all 
-
-        render json: RecipeSerializer.new(recipes)
+        
+        render json: recipes
     end
 
     def show 
-        
+        recipes = Ingredient.find_by(name: params[:ingredient_name]).recipes
+
+        render json: recipes
     end
 
     def create
         recipe = Recipe.new(recipe_params)
-        byebug
+        # byebug
+        ingredients = params[:ingredients].map { |ingredient| Ingredient.find_or_create_by(name: ingredient) }
+        recipe.ingredients << ingredients
+        recipe.save
+        render json: recipe
     end
 
     private
