5fcde9c7cc666e3fa7fd073cae0f2521828c130d
github username <github email address>
2020-07-19 17:23:03 -0700

    added createRecipe to fetch post request

--- phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
+++ phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
@@ -7,4 +7,25 @@ class RecipesAdapter {
         return fetch(this.baseUrl).then(res => res.json()
         )
     }
+
+    createRecipe() {
+
+        return fetch(`${this.baseUrl}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "appplication/json"
+            },
+            body: JSON.stringify({
+                title: event.target[0].value,
+                recipe_link: event.target[1].value,
+                image_link: event.target[2].value,
+                recipe_origin: event.target[3].value,
+                recipe_type: event.target[4].value,
+                ingredients: event.target[5].value
+            })
+        })
+        .then(res => res.json()
+        )
+    }
 }
