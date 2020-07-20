f3f875eaaa8210c461cea283b1456fe9fd11c8d8
github username <github email address>
2020-07-19 22:07:05 -0700

    title case values

--- phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
+++ phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
@@ -2,14 +2,13 @@ class RecipesAdapter {
     constructor() {
         this.baseUrl = 'http://localhost:3000/recipes'
     }
-
+    
     getRecipes() {
         return fetch(this.baseUrl).then(res => res.json()
         )
     }
-
+    
     createRecipe() {
-
         return fetch(`${this.baseUrl}`, {
             method: "POST",
             headers: {
@@ -17,12 +16,12 @@ class RecipesAdapter {
                 "Accept": "appplication/json"
             },
             body: JSON.stringify({
-                title: event.target[0].value,
+                title: titleCase(event.target[0].value),
                 recipe_link: event.target[1].value,
                 image_link: event.target[2].value,
-                recipe_origin: event.target[3].value,
-                recipe_type: event.target[4].value,
-                ingredients: event.target[5].value
+                recipe_origin: titleCase(event.target[3].value),
+                recipe_type: titleCase(event.target[4].value),
+                ingredients: event.target[5].value.split(', ')
             })
         })
         .then(res => res.json()
