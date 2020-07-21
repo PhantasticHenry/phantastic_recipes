bc4a44b2e957ddbbdf8f7494a7b348d7193c1e52
github username <github email address>
2020-07-20 18:16:00 -0700

    added method to populate and sort through all ingredients list

--- phantastic_recipes_frontend/src/components/ingredients.js
+++ phantastic_recipes_frontend/src/components/ingredients.js
@@ -1,5 +1,23 @@
 class Ingredients {
     constructor() {
-        this.adapter = new IngredientsAdapter;
+        this.adapter = new IngredientsAdapter();
+        this.fetchAndLoadIngredients();
     }
+
+    fetchAndLoadIngredients() {
+        this.adapter.getIngredients().then(data => this.sortedIngredientsList(data))
+    }
+
+    sortedIngredientsList(data) {
+        const div = document.querySelector('#all-ingredients')
+        const ul = document.createElement('ul')
+        data.sort((a, b) => (a.name > b.name) ? 1 : -1)
+        for (let ingredient of data) {
+            const li = document.createElement('li')
+            li.innerText = ingredient.name
+            ul.appendChild(li)
+        }
+        div.appendChild(ul)
+    }
+    
 }
