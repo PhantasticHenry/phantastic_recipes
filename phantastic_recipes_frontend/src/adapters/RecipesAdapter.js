fa47bc386aab20cfcea5a3db76d514209531ae23
github username <github email address>
2020-07-22 22:15:54 -0700

    fixed error value of null

--- phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
+++ phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
@@ -4,27 +4,33 @@ class RecipesAdapter {
     }
     
     getRecipes() {
-        return fetch(this.baseUrl).then(res => res.json()
-        )
+        return fetch(this.baseUrl).then(res => (res.json()))
     }
     
-    createRecipe() {
+    createRecipe(form) {
         return fetch(`${this.baseUrl}`, {
+            
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "appplication/json"
             },
             body: JSON.stringify({
-                title: titleCase(event.target[0].value),
-                recipe_link: event.target[1].value,
-                image_link: event.target[2].value,
-                recipe_origin: titleCase(event.target[3].value),
-                recipe_type: titleCase(event.target[4].value),
-                ingredients: event.target[5].value.split(', ')
+                // title: form[0].value,
+                // recipe_link: form[1].value,
+                // image_link: form[2].value,
+                // recipe_origin: form[3].value,
+                // recipe_type: form[4].value,
+                // ingredients: ingredients
+                title: titleCase(form[0].value),
+                recipe_link: form[1].value,
+                image_link: form[2].value,
+                recipe_origin: titleCase(form[3].value),
+                recipe_type: titleCase(form[4].value),
+                ingredients: form[5].value.split(', ')
             })
         })
         .then(res => res.json()
-        )
+        ).catch((error) => alert(res.message))
     }
 }
