746675c720e93b7e8215f969766fc62268d0ba5c
github username <github email address>
2020-07-24 14:24:32 -0700

    created getIngredientRecipes method to grab all recipes belong to a selected ingredient

--- phantastic_recipes_frontend/src/components/recipes.js
+++ phantastic_recipes_frontend/src/components/recipes.js
@@ -1,34 +1,54 @@
 class Recipes {
     constructor() {
         this.recipes = [];
-        this.recipesIngredients = [];
         this.recipesAdapter = new RecipesAdapter();
         this.listOfIngredients = new Ingredients();
-        this.viewAll = document.querySelector('#viewAll')
-        this.addRecipeBtn = document.querySelector('#add-recipe-button')
-        this.allIngredients = document.querySelector('#ingredients-btn')
+        this.viewAll = document.querySelector('#viewAll');
+        this.addRecipeBtn = document.querySelector('#add-recipe-button');
+        this.ingredientsBtn = document.querySelector('#ingredients-btn');
+        this.ingredientsSelect = document.querySelector('#ingredients-select');
+        // this.listOfIngredients = document.querySelector('#all-ingredients');
+        // this.ingredientsContainer = document.querySelector('ingredients-container')
         this.initBindingsAndEventListeners();
         this.fetchAndLoadRecipes();
+        this.getIngredientRecipes();
     }
     
     initBindingsAndEventListeners() {
         this.viewAll.addEventListener('click', this.toggleCardContainer.bind(this));
-        this.allIngredients.addEventListener('click', this.toggleListOfIngredients.bind(this));
-        
+        this.ingredientsBtn.addEventListener('click', this.toggleIngredientsSelect.bind(this));
+        // this.ingredientsBtn.addEventListener('click', this.toggleListOfIngredients.bind(this));
+        //not sure how to create ingredient recipes cards
+
         this.addRecipeBtn.addEventListener('click', function(event) {
             event.preventDefault();
             this.recipesAdapter.createRecipe(event.target.parentElement);
             toggleDisplay('#addRecipeForm');
         }.bind(this))
 
+        addEventListener('onchange', function(e) {
+            console.log(e.target)
+            
+        })
+        this.ingredientsSelect.addEventListener('change', function(e) {
+            const ingredient = e.target.value;
+            // debugger
+            this.getIngredientRecipes(ingredient);
+            console.log(this.ingredientRecipes);
+            
+            
+        }.bind(this))
+       
+        // not working
+        // this.listOfIngredients.addEventListener('click', function(event) {
+        //     let ingredient = event.target;
+        //     this.renderRecipesByIngredient(ingredient);
+        // })
+
     }
 
-    toggleHidden(element) {
-        if (element.classList.contains("hidden")) {
-            element.classList.remove("hidden");
-        } else {
-            element.className += " hidden";
-        }
+    handleIngredientClick(e) {
+        const li = e.target
     }
     
     
@@ -46,18 +66,25 @@ class Recipes {
             recipe.createRecipeCard()            
         }
     }
-    toggleListOfIngredients() {
-        toggleDisplay('#all-ingredients');
+    toggleIngredientsSelect() {
+        toggleDisplay('#ingredients-select');
     }
+    // toggleListOfIngredients() {
+    //     toggleDisplay('#all-ingredients');
+    // }
 
     toggleCardContainer() {
         toggleDisplay('#card-container');
     }
-    
-    // toggleDisplay(id) {
-    //     document.querySelector(id).style.display === "none" ? document.querySelector(id).style.display = "flex" : document.querySelector(id).style.display = "none";
-    // }
 
+    getIngredientRecipes(ingredient) {
+        this.ingredientRecipes = [];
+        for(let recipe of this.recipes) {
+            for(let recipeIngredient of recipe.ingredients) {
+                (recipeIngredient.id == ingredient) && this.ingredientRecipes.push(recipe)
+            }
+        }
+    }
 
 }
 
