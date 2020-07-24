7d2cc4f3128d88dcd02e6bfa12b623e8879ae75a
github username <github email address>
2020-07-23 20:21:13 -0700

    added event listeners to open close list of recipe ingredients

--- phantastic_recipes_frontend/src/components/recipe.js
+++ phantastic_recipes_frontend/src/components/recipe.js
@@ -13,53 +13,41 @@ class Recipe {
         const ingredientsPopup = document.createElement('div')
         const ul = document.createElement('ul')
         const close = document.createElement('button')
-        // const viewAll = document.querySelector('#viewAll')
-        // viewAll.appendChild(ingredientsContainer)
         
         card.setAttribute('class', 'recipe-card')
         ingredientsBtn.setAttribute('id', `recipeIngredients-${this.id}`)
         ingredientsBtn.setAttribute('class', 'recipe-ingredients-open-button')
-        ingredientsBtn.setAttribute('onclick', `openRecipeIngredients(${this.id})`)
-        //dry up ingredietns list code 
-
-        // console.log('testing', `${recipes.toggleDisplay()}`);
+        ingredientsBtn.addEventListener('click', () => document.querySelector(`#ingredientsPopup-${this.id}`).style.display = "block")
+        ingredientsContainer.setAttribute('id', `ingredientsPopup-${this.id}`)
+        ingredientsContainer.setAttribute('class', 'recipe-ingredients-popup')
+        ingredientsPopup.setAttribute('class', 'recipe-ingredients-container')
+        ingredientsBtn.appendChild(ingredientsPopup)
         
         title.innerHTML = this.title
         link.href = this.recipe_link
         img.src = this.image_link
+        ingredientsBtn.innerText = 'Ingredients'
+        
         card.appendChild(title)
         card.appendChild(link)
         link.appendChild(img)
-        
-        ingredientsBtn.innerText = 'Ingredients'
         card.appendChild(ingredientsBtn)
+        card.appendChild(ingredientsContainer)
         
-        ingredientsContainer.setAttribute('id', `ingredientsPopup-${this.id}`) // change this
-        ingredientsContainer.setAttribute('class', 'recipe-ingredients-popup')
-        // ingredients.appendChild(ingredientsContainer)
-        card.appendChild(ingredientsContainer) // this needs
-        
-        ingredientsPopup.setAttribute('class', 'recipe-ingredients-container')
-        ingredientsBtn.appendChild(ingredientsPopup)
-        // ingredientsContainer.appendChild(ingredientsPopup)
-        //ingredients popup content
         for(let ingredients of this.ingredients) {
             const li = document.createElement('li')
             li.innerHTML = ingredients.name
             ul.appendChild(li)
         }
 
-        ingredientsContainer.appendChild(ul)
-        //close ingredients popup
-        // close.setAttribute('id', 'closePopup')
         close.setAttribute('type', 'button')
-        // close.setAttribute('id', 'closeIngredients')
         close.setAttribute('class', 'recipe-ingredients-remove-button')
-        close.addEventListener('click', () => {           
-            document.getElementById(`ingredientsPopup-${this.id}`).style.display = "none"
-        })
+        close.addEventListener('click', () => document.querySelector(`#ingredientsPopup-${this.id}`).style.display = "none")
         close.innerText = 'Close'
+
+        ingredientsContainer.appendChild(ul)
         ingredientsContainer.appendChild(close)
         document.querySelector('.recipe-card-container').appendChild(card)
-    }
+    }   
 }
+        
