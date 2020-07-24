652e6e58f8edfc5e79754e9690a0c03d0f331ebc
github username <github email address>
2020-07-23 22:35:24 -0700

    setAttributes for list of sorted ingredients

--- phantastic_recipes_frontend/src/components/ingredients.js
+++ phantastic_recipes_frontend/src/components/ingredients.js
@@ -1,23 +1,39 @@
 class Ingredients {
     constructor() {
         this.adapter = new IngredientsAdapter();
+        this.ingredientRecipesArray = [];
         this.fetchAndLoadIngredients();
     }
 
     fetchAndLoadIngredients() {
         this.adapter.getIngredients().then(data => this.sortedIngredientsList(data))
     }
-
+    
     sortedIngredientsList(data) {
-        const div = document.querySelector('#all-ingredients')
-        const ul = document.createElement('ul')
+        const div = document.createElement('div')
+        div.setAttribute('id', 'all-ingredients')
+        div.setAttribute('class', 'open-button fade-in')
+        div.setAttribute('style', 'display: none')
+        
+        const recipesContainer = document.createElement('div')
+        const ingredientsUl = document.createElement('ul')
+        const recipesUl = document.createElement('ul')
+
         data.sort((a, b) => (a.name > b.name) ? 1 : -1)
         for (let ingredient of data) {
-            const li = document.createElement('li')
-            li.innerText = ingredient.name
-            ul.appendChild(li)
+            const ingredientsLi = document.createElement('li')
+            ingredientsLi.innerText = `${ingredient.name}   >`
+            ingredientsUl.appendChild(ingredientsLi)
+            for (let recipe of ingredient.recipes) {
+                const recipeLi = document.createElement('li')
+                recipeLi.innerText = recipe.title
+                recipesUl.appendChild(recipeLi)
+            }
         }
-        div.appendChild(ul)
+        div.appendChild(ingredientsUl)
+        document.body.appendChild(div)  
+        recipesContainer.appendChild(recipesUl)
+        document.body.appendChild(recipesContainer)
     }
     
 }
