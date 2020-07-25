f6dd8bc3e5ca5cfccdd567dce21e6b76396683d9
github username <github email address>
2020-07-24 21:30:52 -0700

    changed ul li to select option dropdown

--- phantastic_recipes_frontend/src/components/ingredients.js
+++ phantastic_recipes_frontend/src/components/ingredients.js
@@ -1,7 +1,6 @@
 class Ingredients {
     constructor() {
         this.adapter = new IngredientsAdapter();
-        this.ingredientRecipesArray = [];
         this.fetchAndLoadIngredients();
     }
 
@@ -10,30 +9,15 @@ class Ingredients {
     }
     
     sortedIngredientsList(data) {
-        const div = document.createElement('div')
-        div.setAttribute('id', 'all-ingredients')
-        div.setAttribute('class', 'open-button fade-in')
-        div.setAttribute('style', 'display: none')
-        
-        const recipesContainer = document.createElement('div')
-        const ingredientsUl = document.createElement('ul')
-        const recipesUl = document.createElement('ul')
-
+        const select = document.querySelector('#ingredients-select')
         data.sort((a, b) => (a.name > b.name) ? 1 : -1)
         for (let ingredient of data) {
-            const ingredientsLi = document.createElement('li')
-            ingredientsLi.innerText = `${ingredient.name}   >`
-            ingredientsUl.appendChild(ingredientsLi)
-            for (let recipe of ingredient.recipes) {
-                const recipeLi = document.createElement('li')
-                recipeLi.innerText = recipe.title
-                recipesUl.appendChild(recipeLi)
-            }
+            const option = document.createElement('option')
+            option.value = ingredient.id
+            option.innerText = ingredient.name
+            select.appendChild(option)
         }
-        div.appendChild(ingredientsUl)
-        document.body.appendChild(div)  
-        recipesContainer.appendChild(recipesUl)
-        document.body.appendChild(recipesContainer)
+        document.body.appendChild(select)
     }
-    
+
 }
