126d0dee956df3c7657eb4ec29dd374260d8e27b
github username <github email address>
2020-07-25 23:08:33 -0700

    validation syntax error fixed

--- phantastic_recipes_backend_api/app/models/recipe.rb
+++ phantastic_recipes_backend_api/app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
-    has_many :recipe_ingredients, dependent: :delete_all 
+    has_many :recipe_ingredients, dependent: :delete_all
     has_many :ingredients, through: :recipe_ingredients
 
     validates :title, :recipe_link, :image_link, :recipe_origin, :recipe_type,
-                :presense => true,
-                :allow_blank => false,
-                :uniqueness => true, { message: "Input already taken" }
+                :presence => true, 
+                :uniqueness => true,
+                :allow_blank => false
 end
