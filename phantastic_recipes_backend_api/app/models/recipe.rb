954e7502f2208b371731a0c045b027d0873bd1fa
github username <github email address>
2020-07-26 23:24:06 -0700

    removed uniequesness for recipe origin and recipe type

--- phantastic_recipes_backend_api/app/models/recipe.rb
+++ phantastic_recipes_backend_api/app/models/recipe.rb
@@ -2,8 +2,6 @@ class Recipe < ApplicationRecord
     has_many :recipe_ingredients, dependent: :delete_all
     has_many :ingredients, through: :recipe_ingredients
 
-    validates :title, :recipe_link, :image_link, :recipe_origin, :recipe_type,
-                :presence => true, 
-                :uniqueness => true,
-                :allow_blank => false
+    validates :title, :recipe_link, :image_link, :recipe_origin, :recipe_type, :presence => true, :allow_blank => false
+    validates :title, :recipe_link, :image_link, :uniqueness => true
 end
