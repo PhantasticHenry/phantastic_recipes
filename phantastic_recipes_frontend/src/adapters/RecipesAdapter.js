1f736c2dc15ef5375bc252a3b6daf4a4bb480fe2
github username <github email address>
2021-11-12 20:13:50 -0800

    prettier auto format

--- phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
+++ phantastic_recipes_frontend/src/adapters/RecipesAdapter.js
@@ -1,30 +1,28 @@
 class RecipesAdapter {
-    constructor() {
-        this.baseUrl = 'http://localhost:3000/recipes'
-    }
-    
-    getRecipes() {
-        return fetch(this.baseUrl).then(res => (res.json()))
-    }
-    
-    createRecipe(form) {
-        return fetch(`${this.baseUrl}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "appplication/json"
-            },
-            body: JSON.stringify({
-                title: titleCase(form[0].value),
-                recipe_link: form[1].value,
-                image_link: form[2].value,
-                recipe_origin: form[3].value.toUpperCase(),
-                recipe_type: titleCase(form[4].value),
-                ingredients: titleCase(form[5].value).split(', ')
-            })
-        })
-        .then(res => res.json())
-        .catch((error) => alert(`Error: ${error}`))
-    }
+	constructor() {
+		this.baseUrl = "http://localhost:3000/recipes";
+	}
 
+	getRecipes() {
+		return fetch(this.baseUrl).then((res) => res.json());
+	}
+
+	createRecipe(form) {
+		return fetch(`${this.baseUrl}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				title: titleCase(form[0].value),
+				recipe_link: form[1].value,
+				image_link: form[2].value,
+				recipe_origin: form[3].value.toUpperCase(),
+				recipe_type: titleCase(form[4].value),
+				ingredients: titleCase(form[5].value).split(", "),
+			}),
+		})
+			.then((res) => res.json())
+			.catch((error) => alert(`Error: ${error}`));
+	}
 }
